5759f49ec515695c3e2fe0824d5bc9e53026c71c
Eric Niemeyer <eric.niemeyer@venturetec.net>
2023-03-31 12:59:34 -0400

    x

--- ts_satisfies.ts
+++ ts_satisfies.ts
@@ -2,20 +2,52 @@ console.log('ts_satisfies.ts');
 
 type Colors = 'red' | 'green' | 'blue';
 
-// Ensure that we have exactly the keys from 'Colors'.
-const firstColors = {
+const first: Record<Colors, unknown> = {
   red: 'yes',
   green: false,
   blue: 'kinda',
   platypus: false,
-  //  ~~~~~~~~~~ error - "platypus" was never listed in 'Colors'.
+};
+
+const second = {
+  red: 'yes',
+  green: false,
+  blue: 'kinda',
+  platypus: false,
 } satisfies Record<Colors, unknown>;
 
-// Ensure that we have exactly the keys from 'Colors'.
-const secondColors: Record<Colors, unknown> = {
+const third = {
   red: 'yes',
   green: false,
   blue: 'kinda',
   platypus: false,
-  //  ~~~~~~~~~~ error - "platypus" was never listed in 'Colors'.
-};
+} satisfies Record<Colors, string>;
+
+const fourth = {
+  red: 'yes',
+  green: false,
+  blue: 'kinda',
+  platypus: false,
+} as Record<Colors, unknown>;
+
+const fifth = {
+  red: 'yes',
+  green: false,
+  blue: 'kinda',
+  platypus: false,
+} as Record<Colors, string>;
+
+first.blue.toUpperCase(); // error, secondColors.blue is unknown
+first.green.toUpperCase(); // error, secondColors.green is unknown
+
+second.blue.toUpperCase(); //ok it's a string
+second.green.toUpperCase(); // error, it's a boolean
+
+third.blue.toUpperCase();
+third.green.toUpperCase();
+
+fourth.blue.toUpperCase();
+fourth.green.toUpperCase();
+
+fifth.blue.toUpperCase();
+fifth.green.toUpperCase();
